17b58ac81f06c2d2d1593129aae8f7d684e64b97
RichardWangg <richardwangg198@gmail.com>
2023-06-30 21:40:22 -0400

    Commit for review

--- redux-client/src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const FETCH_ALBUMS = "FETCH_ALBUMS";
-export const FETCH_SONGS = "FETCH_SONGS";
--- redux-client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import albumReducer from "./albumReducer";
-import songReducer from "./songReducer";
-
-export default combineReducers({
-  albums: albumReducer,
-  songs: songReducer,
-});
